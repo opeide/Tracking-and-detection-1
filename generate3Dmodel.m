3bba499686acfedfb479183c7b148e15f5d24154
opeide <fredrikopeide@gmail.com>
2017-12-11 23:43:11 +0100

    mark out region of interest

--- generate3Dmodel.m
+++ generate3Dmodel.m
@@ -1,36 +1,46 @@
-[vertex, face] = read_ply('data/model/teabox.ply');
-
+%init camera
 FX = 2960.37845;
 FY = FX;
 CX = 1841.68855;
 CY = 1235.23369;
-
 IntrinsicMatrix = [FX 0 0; 0 FY 0; CX CY 1];
 cameraParams = cameraParameters('IntrinsicMatrix',IntrinsicMatrix);
 
+%load 3d model
+[vertex, face] = read_ply('data/model/teabox.ply');
+
+%load training images
 trainImgs = dir('data/images/init_texture/*.JPG');
 nImgs = length(trainImgs);
 
+%Map training images to 3d model
 for i=2:2
    currentFilename = trainImgs(i).name;
    currentImg = imread(strcat('data/images/init_texture/', currentFilename));
    currentImg = rgb2gray(currentImg);
    imshow(currentImg);
+
+   
+   
+   %let user select corners
    [xf, yf] = getpts;
    close all
    
+
+   
+   
    nImgPoints = length(xf);
-   
-   cropSize = 15;
-   
-   cornerPoint = struct('img', cell(1, nImgPoints));
-                    
+   %init empty
+   cornerPoint = struct('img', cell(1, nImgPoints));                  
    cornerPos = zeros(nImgPoints, 2);
    cornerIndex = zeros(nImgPoints, 1);
-                    
+   
+   %pixels radius (square) to search for corner
+   cropSize = 15;
+   %create SHIFT descriptors for selected corners
    h = figure;
    for j=1:nImgPoints
-       xi = int32(xf(j) + 0.5);
+       xi = int32(xf(j) + 0.5);     %rund opp
        yi = int32(yf(j) + 0.5);
        
        cropRect = [xi - cropSize, yi - cropSize, 2*cropSize, 2*cropSize];
@@ -47,19 +57,22 @@ for i=2:2
        plot(corner);
        hold off;
    end
-   waitfor(h);
+   waitfor(h);  %until figure closed
+   
+   
    
    h = figure;
    imshow(currentImg);
    hold on;
    
+    %plot initial corner placement
    for j=1:nImgPoints
        x = cornerPos(j, 1);
        y = cornerPos(j, 2);
        plot(x, y, 'r*');
        text(double(x)+10, double(y)+10, cellstr(num2str(j)), 'FontSize',32);
    end
-   
+   %correct corner placement
    indecies_input = inputdlg('Enter vertex indecies:', 'Vertecies', [1 50]);
    indecies = str2num(indecies_input{:});
    
@@ -74,6 +87,7 @@ for i=2:2
    imshow(currentImg);
    hold on;
    
+   
    % Show image with mesh indecies
    for j=1:nImgPoints
        x = cornerPos(j, 1);
@@ -84,8 +98,25 @@ for i=2:2
    waitfor(h);
    
    cornerWorldPos = vertex(indecies,:);
+       figure(3)
+      %mask chosen region
+    corners_x = cornerPos(:,1)
+    corners_y = cornerPos(:,2)
+    poly_indexes = convhull(corners_x, corners_y)
+    mask = roipoly(currentImg, corners_x(poly_indexes), corners_y(poly_indexes));
+    imshow(uint8(mask).*currentImg)
    
    [worldOrientation,worldLocation] = estimateWorldCameraPose(cornerPos,cornerWorldPos,cameraParams,...
        'MaxNumTrials', 10000, 'Confidence', 95, 'MaxReprojectionError', 50);
-   %error when max reprojection under 1784
+   
+
+   
 end
+
+
+
+
+
+
+
+
