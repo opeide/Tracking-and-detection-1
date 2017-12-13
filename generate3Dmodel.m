f5c108a820c29763f55f17cbe4733547c6122371
opeide <fredrikopeide@gmail.com>
2017-12-13 16:01:23 +0100

    pose estimation from texture data

--- generate3Dmodel.m
+++ generate3Dmodel.m
@@ -1,175 +1,175 @@
-run('~/matlab2017b/toolbox/vlfeat-0.9.20/toolbox/vl_setup');
+function [model_descriptors, descriptor_locations] = generate3Dmodel()
 
-%init camera
-FX = 2960.37845;
-FY = FX;
-CX = 1841.68855;
-CY = 1235.23369;
-IntrinsicMatrix = [FX 0 0; 0 FY 0; CX CY 1];
-cameraParams = cameraParameters('IntrinsicMatrix',IntrinsicMatrix);
+    run('~/matlab2017b/toolbox/vlfeat-0.9.20/toolbox/vl_setup');
 
-%load 3d model
-[vertex, face] = read_ply('data/model/teabox.ply');
+    %init camera
+    FX = 2960.37845;
+    FY = FX;
+    CX = 1841.68855;
+    CY = 1235.23369;
+    IntrinsicMatrix = [FX 0 0; 0 FY 0; CX CY 1];
+    cameraParams = cameraParameters('IntrinsicMatrix',IntrinsicMatrix);
 
-%load training images
-trainImgs = dir('data/images/init_texture/*.JPG');
-nImgs = length(trainImgs);
+    %load 3d model
+    [vertex, face] = read_ply('data/model/teabox.ply');
 
-
-model_descriptors = [];
-descriptor_locations = [];
+    %load training images
+    trainImgs = dir('data/images/init_texture/*.JPG');
+    nImgs = length(trainImgs);
 
 
-%Map training images to 3d model
-for i=1:1
-   currentFilename = trainImgs(i).name;
-   currentImg = imread(strcat('data/images/init_texture/', currentFilename));
-   currentImg = rgb2gray(currentImg);
-   imshow(currentImg);
+    model_descriptors = [];
+    descriptor_locations = [];
 
-   
-   
-   %let user select corners
-   [xf, yf] = getpts;
-   close all
-   
 
-   
-   
-   nImgPoints = length(xf);
-   %init empty
-   cornerPoint = struct('img', cell(1, nImgPoints));                  
-   cornerPos = zeros(nImgPoints, 2);
-   cornerIndex = zeros(nImgPoints, 1);
-   
-   %pixels radius (square) to search for corner
-   cropSize = 15;
-   %increase corner accuracy with harris detector
-   h = figure;
-   for j=1:nImgPoints
-       xi = int32(xf(j) + 0.5);     %rund opp
-       yi = int32(yf(j) + 0.5);
-       
-       cropRect = [xi - cropSize, yi - cropSize, 2*cropSize, 2*cropSize];
-       cornerPoint(j).img = imcrop(currentImg, cropRect);
-       corners = detectHarrisFeatures(cornerPoint(j).img);
-       corner = corners.selectStrongest(1);
-       localPos = corner.Location;
-       cornerPos(j, :) = [localPos(1) + single(cropRect(1))  localPos(2) + single(cropRect(2))];
-       
-       
-       subplot(3, 3, j);
-       imshow(cornerPoint(j).img);
+    %Map training images to 3d model
+    for i=1:nImgs
+       currentFilename = trainImgs(i).name;
+       currentImg = imread(strcat('data/images/init_texture/', currentFilename));
+       currentImg = rgb2gray(currentImg);
+       imshow(currentImg);
+
+
+
+       %let user select corners
+       [xf, yf] = getpts;
+       close all
+
+
+
+
+       nImgPoints = length(xf);
+       %init empty
+       cornerPoint = struct('img', cell(1, nImgPoints));                  
+       cornerPos = zeros(nImgPoints, 2);
+       cornerIndex = zeros(nImgPoints, 1);
+
+       %pixels radius (square) to search for corner
+       cropSize = 15;
+       %increase corner accuracy with harris detector
+       h = figure;
+       for j=1:nImgPoints
+           xi = int32(xf(j) + 0.5);     %rund opp
+           yi = int32(yf(j) + 0.5);
+
+           cropRect = [xi - cropSize, yi - cropSize, 2*cropSize, 2*cropSize];
+           cornerPoint(j).img = imcrop(currentImg, cropRect);
+           corners = detectHarrisFeatures(cornerPoint(j).img);
+           corner = corners.selectStrongest(1);
+           localPos = corner.Location;
+           cornerPos(j, :) = [localPos(1) + single(cropRect(1))  localPos(2) + single(cropRect(2))];
+
+
+           subplot(3, 3, j);
+           imshow(cornerPoint(j).img);
+           hold on;
+           plot(corner);
+           hold off;
+       end
+       waitfor(h);  %until figure closed
+
+
+
+       h = figure;
+       imshow(currentImg);
        hold on;
-       plot(corner);
-       hold off;
-   end
-   waitfor(h);  %until figure closed
-   
-   
-   
-   h = figure;
-   imshow(currentImg);
-   hold on;
-   
-    %plot initial corner placement
-   for j=1:nImgPoints
-       x = cornerPos(j, 1);
-       y = cornerPos(j, 2);
-       plot(x, y, 'r*');
-       text(double(x)+10, double(y)+10, cellstr(num2str(j)), 'FontSize',32);
-   end
-   %correct corner placement
-   indecies_input = inputdlg('Enter vertex indecies:', 'Vertecies', [1 50]);
-   indecies = str2num(indecies_input{:});
-   
-   assert(length(indecies) == nImgPoints, 'Wrong number of indecies');
-   
-   for j=1:length(indecies)
-       cornerIndex(j) = indecies(j);
-   end
-   close all
-   
-   h = figure;
-   imshow(currentImg);
-   hold on;
-   
-   
-   % Show image with mesh indecies
-   for j=1:nImgPoints
-       x = cornerPos(j, 1);
-       y = cornerPos(j, 2);
-       plot(x, y, 'r*');
-       text(double(x)+10, double(y)+10, cellstr(num2str(cornerIndex(j))), 'FontSize',32);
-   end
-   waitfor(h);
-   
 
-   %PnP to decide camera-world mapping
-   cornerWorldPos = vertex(indecies,:);
-  [worldOrientation,worldLocation] = estimateWorldCameraPose(cornerPos,cornerWorldPos,cameraParams,...
-   'MaxNumTrials', 10000, 'Confidence', 99, 'MaxReprojectionError', 5)
+        %plot initial corner placement
+       for j=1:nImgPoints
+           x = cornerPos(j, 1);
+           y = cornerPos(j, 2);
+           plot(x, y, 'r*');
+           text(double(x)+10, double(y)+10, cellstr(num2str(j)), 'FontSize',32);
+       end
+       %correct corner placement
+       indecies_input = inputdlg('Enter vertex indecies:', 'Vertecies', [1 50]);
+       indecies = str2num(indecies_input{:});
 
-    P = IntrinsicMatrix'*cat(2, (worldOrientation), -worldOrientation*worldLocation');
+       assert(length(indecies) == nImgPoints, 'Wrong number of indecies');
 
-    %Map 3d model vertex to image with the PnP result
-    h = figure;
-    imshow(currentImg)
-    hold on
-    pos = worldToImage(cameraParams , inv(worldOrientation), -worldOrientation*worldLocation', vertex);
-    plot(pos(:,1), pos(:,2), 'r+');
-    waitfor(h);
-    
-    
-    %extract ROI
-    corners_x = cornerPos(:,1);
-    corners_y = cornerPos(:,2);
-    poly_indexes = convhull(corners_x, corners_y);
-    minx = min(corners_x(poly_indexes));
-    miny = min(corners_y(poly_indexes));
-    maxx = max(corners_x(poly_indexes));
-    maxy = max(corners_y(poly_indexes));
-    rect = [minx, miny, maxx-minx, maxy-miny];
-    img_roi = imcrop(currentImg, rect);
-    
-    figure(3)
-    imshow(currentImg)
-    hold on;
+       for j=1:length(indecies)
+           cornerIndex(j) = indecies(j);
+       end
+       close all
 
-    %Detect features in ROI
-    roi_offset = [minx miny];
+       h = figure;
+       imshow(currentImg);
+       hold on;
 
-    peak_thresh = 7;
-    img_roi = single(img_roi);
-    [frame, desc] = vl_sift(img_roi, 'PeakThresh', peak_thresh);
-    num_features = size(frame);
-    num_features = num_features(2);
-    for k = 1:10
-        k
-        pos_roi = frame(1:2, k)'
-        pos = roi_offset + pos_roi;
-        plot(pos(1), pos(2), 'g*');
-        [world_coords, correct] = pix2world(pos, IntrinsicMatrix, worldOrientation, worldLocation, face, vertex);
-        if correct == 1
-            scale = frame(3, k)
-            thresh = frame(4, k)
-            proj = P*cat(2, world_coords, 1)';
-            proj = proj(1:2)/proj(3);
-            plot(proj(1), proj(2), 'r+');
-            
-            model_descriptors = [model_descriptors descriptor];
-            descriptor_locations = [descriptor_locations world_coords];
+
+       % Show image with mesh indecies
+       for j=1:nImgPoints
+           x = cornerPos(j, 1);
+           y = cornerPos(j, 2);
+           plot(x, y, 'r*');
+           text(double(x)+10, double(y)+10, cellstr(num2str(cornerIndex(j))), 'FontSize',32);
+       end
+       waitfor(h);
+
+
+       %PnP to decide camera-world mapping
+       cornerWorldPos = vertex(indecies,:);
+      [worldOrientation,worldLocation] = estimateWorldCameraPose(cornerPos,cornerWorldPos,cameraParams,...
+       'MaxNumTrials', 10000, 'Confidence', 99, 'MaxReprojectionError', 5)
+
+        P = IntrinsicMatrix'*cat(2, (worldOrientation), -worldOrientation*worldLocation');
+
+        %Map 3d model vertex to image with the PnP result
+        h = figure;
+        imshow(currentImg)
+        hold on
+        pos = worldToImage(cameraParams , inv(worldOrientation), -worldOrientation*worldLocation', vertex);
+        plot(pos(:,1), pos(:,2), 'r+');
+        waitfor(h);
+
+
+        %extract ROI
+        corners_x = cornerPos(:,1);
+        corners_y = cornerPos(:,2);
+        poly_indexes = convhull(corners_x, corners_y);
+        minx = min(corners_x(poly_indexes));
+        miny = min(corners_y(poly_indexes));
+        maxx = max(corners_x(poly_indexes));
+        maxy = max(corners_y(poly_indexes));
+        rect = [minx, miny, maxx-minx, maxy-miny];
+        img_roi = imcrop(currentImg, rect);
+
+        figure(3)
+        imshow(currentImg)
+        hold on;
+
+        %Detect features in ROI
+        roi_offset = [minx miny];
+
+        peak_thresh = 7;
+        img_roi = single(img_roi);
+        [frame, desc] = vl_sift(img_roi, 'PeakThresh', peak_thresh);
+        num_features = size(frame);
+        num_features = num_features(2);
+        for k = 1:num_features
+            pos_roi = frame(1:2, k)';
+            pos = roi_offset + pos_roi;
+            plot(pos(1), pos(2), 'g*');
+            [world_coords, correct] = pix2world(pos, IntrinsicMatrix, worldOrientation, worldLocation, face, vertex);
+            if correct == 1
+                scale = frame(3, k);
+                thresh = frame(4, k);
+                proj = P*cat(2, world_coords, 1)';
+                proj = proj(1:2)/proj(3);
+                plot(proj(1), proj(2), 'r+');
+                
+                descriptor = desc(:,k);
+                model_descriptors = [model_descriptors descriptor];
+                descriptor_locations = [descriptor_locations world_coords'];
+            end
+
         end
-        
-    end
-    
-
-
-   
-end
 
 
 
+
+    end%loop end
+end%func end
 
 
 
--- poseEstimation.m
+++ poseEstimation.m
@@ -1,8 +1,17 @@
 
 
+%generate model texture
+%[model_desc, model_desc_loc] = generate3Dmodel();
 
+[vertex, face] = read_ply('data/model/teabox.ply');
 
-
+%init camera
+FX = 2960.37845;
+FY = FX;
+CX = 1841.68855;
+CY = 1235.23369;
+IntrinsicMatrix = [FX 0 0; 0 FY 0; CX CY 1];
+cameraParams = cameraParameters('IntrinsicMatrix',IntrinsicMatrix);
 
 %generating camera poses for all images in images/detection
 detectImgs = dir('data/images/detection/*.JPG');
@@ -14,15 +23,46 @@ highestNrInliersImgID = 0;
 %filtering out the best hypothesis for each pose
 %where to ransac?
 
-for i=1:1
+for i=16:nImgs
    currentFilename = detectImgs(i).name;
    currentImg = imread(strcat('data/images/detection/', currentFilename));
    currentImg = rgb2gray(currentImg);
-   figure(1);
+   h = figure(1);
    imshow(currentImg);
+   hold on
+   
+   %find features in image
+    peak_thresh = 5;
+    currentImg = single(currentImg);
+    [frame, desc] = vl_sift(currentImg, 'PeakThresh', peak_thresh);
+    num_features = size(frame);
+    num_features = num_features(2);
+    
+    %match found features with known model meatures
+    [matches, scores] = vl_ubcmatch(desc, model_desc);
+%     indexes = matches(1, :);
+%     model_indexes = matches(2, :);
+    
+    %get best topPart (fraction of all) matches
+    topPart = 0.9;
+    [sortedValues,sortIndex] = sort(scores(:),'ascend');  %# Sort the values in
+    bestMatches = sortIndex(1:topPart*length(sortIndex));
+    indexes = matches(1, bestMatches);
+    model_indexes = matches(2, bestMatches);
+    plot(frame(1, indexes), frame(2, indexes), 'r+')
+    
 
-%    %compute camera orientation and location
-%    [worldOrientation, worldLocation, inliersID] = estimateWorldCameraPose(data.imagePoints, data.worldPoints, data.cameraParams);
+   %2d and 3d position of found features
+   pix_match_loc = frame(1:2, indexes); 
+   model_match_loc = model_desc_loc(:, model_indexes);
+
+   %compute camera pose from 3d-2d correspondences
+   [worldOrientation, worldLocation, inliersID] = estimateWorldCameraPose(pix_match_loc', model_match_loc', cameraParams, ...
+       'MaxNumTrials', 100000, 'Confidence', 99, 'MaxReprojectionError', 2);
+   
+   pos = worldToImage(cameraParams , inv(worldOrientation), -worldOrientation*worldLocation', vertex);
+   plot(pos(:,1), pos(:,2), 'g*');
+   waitfor(h)
 %    
 %    %plot
 %    pcshow(data.worldPoints,'VerticalAxis','Y','VerticalAxisDir','down','MarkerSize',30);
